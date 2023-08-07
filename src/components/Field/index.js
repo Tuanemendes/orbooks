ebfe4c77ac0c3cd109db36ff09ae96067baf128f
Tuanemendes <tuane.belmiro@gmail.com>
2023-08-07 17:59:50 -0300

    alteração no css dos campo

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -4,7 +4,7 @@ import styles from './style.module.css'
 const Field = (props) => {
   return (
     <div className={styles.container}>
-        <label for="">{props.label}</label>
+        <label>{props.label}</label>
         <input placeholder={props.placeholder} type="text" name="name" id="name-input"/>
     </div>
   )
